8e8ad4b3a97c908c4385ce15dc0d3b386c0210fd
bizbuddydev <bizbuddydev@gmail.com>
2024-12-10 20:32:46 -0800

    Update ig_buddy.py

--- ig_buddy.py
+++ ig_buddy.py
@@ -3,6 +3,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
 from datetime import date, timedelta
+import config
 
 st.set_page_config(page_title="Social Overview", layout="wide")
 
